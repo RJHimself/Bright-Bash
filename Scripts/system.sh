497eda38d8a3f59eaf4a811d0e7f85c309ad225c
RJ <rjseven007@gmail.com>
2019-10-17 03:43:18 +0100

    Changes

--- Scripts/system.sh
+++ Scripts/system.sh
@@ -147,12 +147,6 @@ function AutoLogin {
     ;;
     esac
 }
-function UnlockSudoPassword {
-    # Sudo - Removing Password Prompt from "sudo"
-
-    sudo sed --in-place "s/\(\#\s\+%sudo\s\+ALL=(ALL)\s\+ALL\)/%sudo ALL=(ALL) NOPASSWD: ALL/" "/etc/sudoers"
-    sudo sed --in-place "s/\#includedir\s\+\/etc\/sudoers.d/#includedir \/etc\/sudoers.d\n%$USER ALL=(ALL) NOPASSWD: ALL/" "/etc/sudoers"
-}
 
 
 function PrintFunction { declare -f "$1"; }
