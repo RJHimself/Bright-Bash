c5675d37ecb9ce09cad537d417e8266b75d920d1
RJ <rjseven007@gmail.com>
2019-10-23 00:17:23 +0100

    Changes

--- Scripts/answers.sh
+++ Scripts/answers.sh
@@ -12,8 +12,10 @@ function IsOFF {
 }
 
 
+function IsDownloadOrUpload { if $(IsDownload "$1") || $(IsUpload "$1"); then echo true; else echo false; fi }
 function IsDownload { [[ "$(FirstUCase "$1")" == "D" ]] && echo true || echo false; }
 function IsUpload { [[ "$(FirstUCase "$1")" == "U" ]] && echo true || echo false; }
 
+function IsNotDownloadOrUpload { $(IsDownloadOrUpload "$1") && echo false || echo true; }
 function IsNotDownload { $(IsDownload "$1") && echo false || echo true; }
 function IsNotUpload { $(IsUpload "$1") && echo false || echo true; }
