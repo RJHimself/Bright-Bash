65e83e50fcdf5ea46e78b5918b221f7bdb2fab92
RJ <rjseven007@gmail.com>
2019-10-28 03:32:20 +0000

    Changes

--- Scripts/text.sh
+++ Scripts/text.sh
@@ -147,6 +147,8 @@ function LacksAny {
 }
 
 
+function IsWord { [[ "$1" == "$2" ]] && echo true || echo false; }
+function IsNotWord { $(IsWord "$@") && echo false || echo true; }
 function IsAny {
     local strEnglobber="$1"
 
