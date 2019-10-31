020d49601f9b67bf76d73ff32009b14653c6f5cf
RJ <rjseven007@gmail.com>
2019-10-31 17:54:46 +0000

    Changes

--- Scripts/system.sh
+++ Scripts/system.sh
@@ -305,28 +305,28 @@ function GenerateDistroVars {
     OS=""
     VER=""
 
-    if [ -f /etc/os-release ]; then
+    if $(FileExists "/etc/os-release"); then
         # freedesktop.org and systemd
-        . /etc/os-release
-        OS=$NAME
-        VER=$VERSION_ID
+        source /etc/os-release
+        OS="$NAME"
+        VER="$VERSION_ID"
     elif type lsb_release >/dev/null 2>&1; then
         # linuxbase.org
         OS=$(lsb_release -si)
         VER=$(lsb_release -sr)
-    elif [ -f /etc/lsb-release ]; then
+    elif $(FileExists "/etc/lsb-release"); then
         # For some versions of Debian/Ubuntu without lsb_release command
-        . /etc/lsb-release
+        source "/etc/lsb-release"
         OS=$DISTRIB_ID
         VER=$DISTRIB_RELEASE
-    elif [ -f /etc/debian_version ]; then
+    elif $(FileExists "/etc/debian_version"); then
         # Older Debian/Ubuntu/etc.
-        OS=Debian
+        OS="Debian"
         VER=$(cat /etc/debian_version)
-    elif [ -f /etc/SuSe-release ]; then
+    elif $(FileExists "/etc/SuSe-release"); then
         # Older SuSE/etc.
         ...
-    elif [ -f /etc/redhat-release ]; then
+    elif $(FileExists "/etc/redhat-release"); then
         # Older Red Hat, CentOS, etc.
         ...
     else
