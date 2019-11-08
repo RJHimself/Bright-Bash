dd53332c0600b3e91ae7996668c471849d5b1f41
RJ <rjseven007@gmail.com>
2019-11-08 18:04:46 +0000

    Changes

--- Scripts/math.sh
+++ Scripts/math.sh
@@ -36,6 +36,24 @@ function GetVersion {
 
     echo "$finalVersion"
 }
+function GetNumber_First {
+    local strEnglobber="$(Trim "$1")"
+    local versionIndex="$(IndexOf_Lowest "$strEnglobber" 1 2 3 4 5 6 7 8 9 0)"
+    local currentIndex=$versionIndex
+    local currentChar="$(GetChar $currentIndex "$strEnglobber")"
+    local finalVersion
+
+
+    while $(IsNumber $currentChar); do
+        finalVersion="$finalVersion""$currentChar"
+
+        let "currentIndex++"
+        currentChar="$(GetChar $currentIndex "$strEnglobber")"
+    done
+
+
+    echo "$finalVersion"
+}
 
 
 function Sort { echo "$(SmlCutLines_Empty "$1")" | sort --version-sort; }
