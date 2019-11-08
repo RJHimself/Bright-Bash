4433175307914231515def9fe26613e61f2a91cc
RJ <rjseven007@gmail.com>
2019-11-08 21:47:25 +0000

    Changes

--- Scripts/files.sh
+++ Scripts/files.sh
@@ -23,7 +23,7 @@ function ExeOnDir {
 
     $(IsEmpty "$dir") && dir="$PWD"
     dir="$(SwitchDirSymbols_Folder "$dir")"
-    CreateFile_IfNotExists "$dir"
+    CreateFolder_IfNotExists "$dir"
 
 
     eval "cd \"$dir\"; $toExe; cd \"$oldDir\""
