deb65b0abb8577d86713a195ce56b4cea065514a
RJ <rjseven007@gmail.com>
2019-11-21 05:38:03 +0000

    2019-11-21 | 05-38-03

--- extra-aliases.sh
+++ extra-aliases.sh
@@ -1,2 +1,4 @@
-alias download_to_folder="wget -P"
-alias download_to_file="wget -O"
+alias "download_to_folder=wget -P"
+alias "download_to_file=wget -O"
+alias "wget -folder=wget -P"
+alias "wget -file=wget -O"
