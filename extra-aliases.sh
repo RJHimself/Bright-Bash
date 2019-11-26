4794d245bd56a146ac7bb55b1262e27f2981a6fa
RJ <rjseven007@gmail.com>
2019-11-26 12:59:13 +0000

    2019-11-26 | 12-59-13

--- extra-aliases.sh
+++ extra-aliases.sh
@@ -2,5 +2,5 @@ alias "download_to_folder=wget -P"
 alias "download_to_file=wget -O"
 
 # Pacman Ease to Use
-alias "pacman install=pacman -S"
-alias "pacman update=pacman -Syu"
+alias "pacman-install=pacman -S"
+alias "pacman-update=pacman -Syu"
